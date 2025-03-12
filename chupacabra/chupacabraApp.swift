dbe35eaff1a138d898a2ac2ffd08ba4fa3eedaee
Robin Hermet <robin.hermet48@gmail.com>
2025-03-12 12:15:23 +0100

    init

---
+++ chupacabra/chupacabraApp.swift
@@ -0,0 +1,17 @@
+//
+//  chupacabraApp.swift
+//  chupacabra
+//
+//  Created by etud on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct chupacabraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
